5f183d1e58fc256ba6085f4772347777a3f3bd73
Alexander Guðmundsson <alexandergud18@gmail.com>
2016-11-17 15:28:23 +0000

    continuously score added

--- Start.py
+++ Start.py
@@ -1,9 +1,36 @@
 import pygame
 import random
 
-
 # If we want to use sprites we create a class that inherits from the Sprite class.
 # Each class has an associated image and a rectangle.
+
+# -----------Variables-----------------
+time = 0
+speed = 2
+score = 0
+life = 3
+player_speed = 6
+asteroid_speed = 1
+Invulnerable = False
+WHITE = (255, 255, 255)
+DARK_GREY = (50, 50, 50)
+
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 400
+# -------------------------------------
+
+# ----------Image Loader---------------
+player_image = pygame.image.load('images/spaceship.png')
+player_imagedark = pygame.image.load('images/spaceship1.png')
+player_imagelight = pygame.image.load('images/spaceship2.png')
+life_image = pygame.transform.scale(player_image, (20, 20))
+asteroid_image = pygame.image.load('images/asteroid.png')
+missile_image = pygame.image.load('images/missile.png')
+
+
+# -------------------------------------
+
+# --------------Classes----------------
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -24,8 +51,9 @@ class Missile(pygame.sprite.Sprite):
         self.image = missile_image
         self.rect = self.image.get_rect()
 
+
 class Background:
-    def Create(self):
+    def create(self):
         global x, y, x1, y1, w, h, background, background_size, background_rect, screen
         background = pygame.image.load('images/space.jpg')
         background_size = background.get_size()
@@ -36,7 +64,8 @@ class Background:
 
         x1 = 0
         y1 = -h
-    def Scroll(self):
+
+    def scroll(self):
         global y1, y, h, background, screen
         y1 += 5
         y += 5
@@ -47,13 +76,27 @@ class Background:
         if y1 > h:
             y1 = -h
 
+    def scoreBoard(self):
+        global score, life
+        scoretext = font.render("Score:" + str(score), 1, (8, 255, 0))
+        pygame.draw.rect(screen, (0, 0, 0), (590, 10, 150, 20))
+        pygame.Surface.blit(screen, scoretext, (590, 10))
+
+    def lifeBoard(self):
+        lifetext = font.render("Life:", 1, (8, 255, 0))
+        pygame.draw.rect(screen, (0, 0, 0), (590, 40, 150, 50))
+        pygame.Surface.blit(screen, lifetext, (590, 40))
+        for x in range(life):
+            pygame.Surface.blit(screen, life_image, ((580 + (x * 25 + 10)), 70))
+
+
 class Spawn:
-    def Asteroids(self):
+    def asteroids(self):
         for i in range(30):
             asteroid = Asteroid()
             # Set a random location for the new asteroid.
             # All the asteroids are within the left / right boundaries of the screen
-            asteroid.rect.x = random.randrange(SCREEN_WIDTH - 20)
+            asteroid.rect.x = random.randrange(550 - 20)
             # make sure the asteroids are first above the visual top and then they
             # appear "from above" :-)
             asteroid.rect.y = -random.randrange(120)
@@ -61,14 +104,16 @@ class Spawn:
             asteroid_list.add(asteroid)
             all_sprites_list.add(asteroid)
 
+
 class Uprades:
-    def Basic(self):
+    def basic(self):
         shot = Missile()
         shot.rect.x = player.rect.x + 17
         shot.rect.y = player.rect.y - 15
         missile_list.add(shot)
         all_sprites_list.add(shot)
-    def DualShot(self):
+
+    def dualShot(self):
         shot = Missile()
         shot.rect.x = player.rect.x + 5
         shot.rect.y = player.rect.y - 15
@@ -79,7 +124,8 @@ class Uprades:
         shot2.rect.y = player.rect.y - 15
         missile_list.add(shot2)
         all_sprites_list.add(shot2)
-    def TripleShot(self):
+
+    def tripleShot(self):
         shot = Missile()
         shot.rect.x = player.rect.x + 17
         shot.rect.y = player.rect.y - 15
@@ -96,19 +142,13 @@ class Uprades:
         missile_list.add(shot3)
         all_sprites_list.add(shot3)
 
+
+# ---------------------------------------------
+
 pygame.init()
-
-# Lets load the game images and put them into variables
-player_image = pygame.image.load('images/spaceship.png')
-asteroid_image = pygame.image.load('images/asteroid.png')
-missile_image = pygame.image.load('images/missile.png')
-
-WHITE = (255, 255, 255)
-DARK_GREY = (50, 50, 50)
-
-SCREEN_WIDTH = 550
-SCREEN_HEIGHT = 400
+pygame.display.set_caption('SpaceRush')
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+font = pygame.font.Font(None, 30)
 
 # This is a list of 'sprites.' Each block in the program is
 # added to this list. The list is managed by a class called 'Group.'
@@ -127,15 +167,17 @@ player.rect.y = 350
 
 all_sprites_list.add(player)
 
-#Creates the background
+# Creates the background
 bg = Background()
-bg.Create()
+bg.create()
+bg.scoreBoard()
+bg.lifeBoard()
 
-#Spawns enemies
+# Spawns enemies
 spawner = Spawn()
-spawner.Asteroids()
+spawner.asteroids()
 
-#Upgrades
+# Upgrades
 upgrade = Uprades()
 Dualshot = False
 Tripleshot = False
@@ -145,66 +187,70 @@ done = False
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-score = 0
 
 # -------- Main Program Loop -----------
 # We need to check out what happens when the player hits the space bar in order to "shoot".
 # A new missile is created and gets it's initial position in the "middle" of the player.
 # Then this missile is added to the missile sprite-group and also to the all_sprites group.
 while not done:
+    time += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if Dualshot == True:
-                    upgrade.DualShot()
-                elif Tripleshot == True:
-                    upgrade.TripleShot()
+                if Dualshot:
+                    upgrade.dualShot()
+                elif Tripleshot:
+                    upgrade.tripleShot()
                 else:
-                    upgrade.Basic()
+                    upgrade.basic()
             if event.key == pygame.K_b:
-                spawner.Asteroids()
+                spawner.asteroids()
             if event.key == pygame.K_d:
-                if Tripleshot != True:
+                if not Tripleshot:
                     Dualshot = True
             if event.key == pygame.K_t:
-                if Dualshot == True:
+                if Dualshot:
                     Tripleshot = True
                     Dualshot = False
                 else:
                     Tripleshot = True
-
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
-        if player.rect.x < -65:
-            player.rect.x = 550
-        player.rect.x -= 5
+        if player.rect.x < 5:
+            player.rect.x = 5
+        player.rect.x -= player_speed
     if key[pygame.K_RIGHT]:
-        if player.rect.x > 550:
-            player.rect.x = - 65
-        player.rect.x += 5
+        if player.rect.x > 540:
+            player.rect.x = 540
+        player.rect.x += player_speed
     if key[pygame.K_UP]:
         if player.rect.y < 0:
             player.rect.y = 0
-        player.rect.y -= 5
+        player.rect.y -= player_speed
     if key[pygame.K_DOWN]:
         if player.rect.y > 350:
             player.rect.y = 350
-        player.rect.y += 5
+        player.rect.y += player_speed
 
-    bg.Scroll()
+    if time % speed == 0:
+        bg.scroll()
 
-    # Below is another good example of Sprite and SpriteGroup functionality.
-    # It is now enough to see if some missile has collided with some asteroid
-    # and if so, they are removed from their respective groups.
-    # In other words:  A missile exploded and so did an asteroid.
+    if time % 10 == 0:
+        score += 1
+        bg.scoreBoard()
 
-    # See if the player block has collided with anything.
-    pygame.sprite.groupcollide(missile_list, asteroid_list, True, True)
+    if pygame.sprite.groupcollide(missile_list, asteroid_list, True, True):
+        score += 10
+        bg.scoreBoard()
     if pygame.sprite.spritecollide(player, asteroid_list, False):
-        all_sprites_list.remove(player)
-        print('Collided')
+        if not Invulnerable:
+            life -= 1
+            print('Collided')
+            bg.lifeBoard()
+            Invulnerable = True
+            all_sprites_list.remove(player)
 
     # Missiles move at a constant speed up the screen, towards the enemy
     for shot in missile_list:
@@ -214,11 +260,11 @@ while not done:
             all_sprites_list.remove(shot)
 
     # All the enemies move down the screen at a constant speed
-    for block in asteroid_list:
-        block.rect.y += 1
-        if block.rect.y == 400:
-            asteroid_list.remove(block)
-            all_sprites_list.remove(block)
+    for comic in asteroid_list:
+        comic.rect.y += asteroid_speed
+        if comic.rect.y == 400:
+            asteroid_list.remove(comic)
+            all_sprites_list.remove(comic)
 
     # Draw all the spites
     all_sprites_list.draw(screen)
